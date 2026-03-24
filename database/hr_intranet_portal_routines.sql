e2bbc03d7ae64553cfcfef55744f22c56a6573ca
Ranjita2022 <ranjitaambali@gmail.com>
2026-03-24 16:43:06 +0530

    DB pushed

--- database/hr_intranet_portal_routines.sql
+++ database/hr_intranet_portal_routines.sql
@@ -170,4 +170,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-24 16:38:27
+-- Dump completed on 2026-03-24 16:42:17
